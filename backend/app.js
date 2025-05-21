811713fbcda1ae002dcd9a984db7e96d93e80c59
surafeljs <surafel.node@gmail.com>
2025-05-21 11:28:56 -0700

    modified

---
+++ backend/app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = require("./router");
+const app = express();
+app.use(router);
+const port = 4000;
+
+app.listen(port, (err) => {
+  console.log(`server is runing http:// ${port}`);
+});
