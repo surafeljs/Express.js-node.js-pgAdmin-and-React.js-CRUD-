bc6d90db213a8b4c17de715402f51bbcd11207e0
surafeljs <surafel.node@gmail.com>
2025-05-21 12:27:29 -0700

    modified

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = require("./router");
 const app = express();
 app.use(router);
+
 const port = 4000;
 
 app.listen(port, (err) => {
--- backend/router.js
+++ backend/router.js
@@ -1,12 +1,32 @@
 const express = require("express");
 const db = require("./database/db");
 const router = express.Router();
+router.use(express.json());
+
 router.get("/", async (req, res) => {
   try {
     const { rows } = await db.query("SELECT * FROM st");
-    res.status(200).send(rows[0]);
+    res.status(200).json(rows);
   } catch (error) {
     res.status(500).json({ error: error.message });
+  }
+});
+
+router.post("/post", async (req, res) => {
+  const { id, name, age } = req.body;
+
+  try {
+    const { rows, err } = await db.query(
+      "INSERT INTO  st(id,name,age)VALUES($1,$2,$3) RETURNING *",
+      [id, name, age]
+    );
+    if (rows) {
+      res.status(200).json(rows[0]);
+    } else {
+      res.status(404).json("fffffff");
+    }
+  } catch (error) {
+    res.status(500).json(error.message);
   }
 });
 module.exports = router;
